8e691fbf480fa479c80d0c22cb8e19522c853bcd
Haziq Othman <haziq@runcloud.io>
2026-04-07 12:15:30 +0800

    Add web and vendor for deployment

---
+++ web/app/themes/haziq_theme/footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <p>Footer section - Haziq Theme</p>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ web/app/themes/haziq_theme/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+  <h2>This is my homepage</h2>
+  <p>Now using header + footer structure</p>
+</main>
+
+<?php get_footer(); ?>
